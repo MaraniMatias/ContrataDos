e250d7078ef16e5066d08f85750612d447f96731
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-03-25 19:41:21 -0300

    Using bcrypt

--- server/models/persona.js
+++ server/models/persona.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
 const ObjectId = mongoose.Schema.Types.ObjectId
+const saltRounds = 10
 
 const PersonaRol = {
   CLIENTE: 'CLIENTE',
@@ -42,14 +44,42 @@ const schema = new Schema(
     puntuacion: Number, // TODO 0 al 10, calcular con algun metodo
     cantidad_trabajos: Number, // TODO Incrementar al terminar un trabajo
     google_account: { type: Object, access: 'protected' }, // Datos de google,
+    password: { type: String, access: 'protected' },
     zona_trabajo: [{ type: ObjectId, ref: 'localidad' }], // libre todo el mundo
     deleted: { type: Boolean, default: false },
   },
   { timestamps: true }
 )
 
-schema.virtual('usuario').get(function () {
+schema.virtual('display_name').get(function () {
   return this.razon_social || this.apellido + ' ' + this.nombre
 })
+
+schema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, saltRounds)
+  } else next()
+})
+
+// Checks password match
+// XXX using awit
+schema.method('authenticate', function (password) {
+  return bcrypt.compare(password, this.password)
+})
+
+/*
+schema.static("findOrCreate", function(condition, user, callback) {
+  const usuario = this;
+  usuario.findOne(condition, (err, result) => {
+    if (err || result) {
+      callback(err, result);
+    } else {
+      usuario.create(user, (err, userBD) => {
+        callback(err, userBD);
+      });
+    }
+  });
+});
+*/
 
 module.exports.Persona = mongoose.model('persona', schema)
