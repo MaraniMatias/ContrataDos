f1033635fba8475aa7aec1a3a09f7cd827296f7d
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-03-30 14:53:25 -0300

    Basic perfil page

--- pages/perfil/_id.vue
+++ pages/perfil/_id.vue
@@ -1,26 +1,51 @@
 <template>
-  <v-layout column pb-12 mb-2 px-2 align-center>
-    <v-flex xs12 sm12 md10 lg10 xl6 mt-4 mb-2>
-      <v-layout>
-        <Avatar size="225" src="/avatars/helen.jpg" />
+  <v-layout row pb-12 mb-2 px-2 justify-center>
+    <v-flex xs12 sm12 md10 lg6 xl6 mt-4 mb-2>
+      <v-layout justify-center align-start>
+        <v-flex xs12 lg4>
+          <v-layout column align-center>
+            <Avatar size="225" :src="perfil.picture" class="ma-2" />
+            <Rating :value="perfil.puntuacion" />
+          </v-layout>
+        </v-flex>
+        <v-flex xs12 lg8>
+          <v-layout column>
+            <div class="overline mt-2">
+              Trabajos: {{ perfil.cantidad_trabajos || 0 }}
+            </div>
+            <p class="headline mb-1" v-text="headline" />
+            <p>Profesiones: {{ perfil.servicios }}</p>
+            <p>{{ perfil.bibliography }}</p>
+            <p>Vive en: {{ perfil.localidad }}</p>
+          </v-layout>
+        </v-flex>
       </v-layout>
-      <pre v-text="perfil" />
+
+      <v-layout justify-center align-start mt-4>
+        <v-divider />
+      </v-layout>
+
+      <v-layout justify-center>
+        <pre v-text="perfil" />
+      </v-layout>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import api from '~/api/baseApi'
 import Avatar from '~/components/Avatar'
+import Rating from '~/components/Rating'
+import ObjectId from '~/utils/formRules/objectId'
+import camelCase from '~/utils/capitalizeWords'
+
+import api from '~/api/baseApi'
 const Persona = api('/Persona')
 
 export default {
   // middleware: 'authenticated', es publico
-  components: { Avatar },
+  components: { Avatar, Rating },
   validate({ params }) {
-    return params.id
-      ? /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i.test(params.id)
-      : true
+    return ObjectId()(params.id) === true
   },
   async asyncData({ params, store }) {
     if (params.id) {
@@ -37,7 +62,21 @@ export default {
     editable() {
       return !this.$route.params.id
     },
+    headline() {
+      return camelCase(
+        this.perfil.razon_social
+          ? this.razon_social + `(${this.perfil.display_name})`
+          : this.perfil.display_name
+      )
+    },
   },
-  methods: {},
+  methods: {
+    changeBiblo() {
+      Persona.save({
+        _id: this.perfil._id,
+        bibliography: '<p>I <b>think</b> this is good.</p>',
+      })
+    },
+  },
 }
 </script>
