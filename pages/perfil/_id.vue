ef23fc9247294bda020fc4fcf774e088dbc3efea
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-03-30 16:56:01 -0300

    Basic layout for show job

--- pages/perfil/_id.vue
+++ pages/perfil/_id.vue
@@ -39,12 +39,76 @@
       <v-layout v-if="!loadingTrabajos" justify-center fill-height mt-0>
         <v-flex xs12>
           <v-hover v-slot:default="{ hover }" open-delay="200">
-            <v-card outlined :elevation="hover ? 1 : 0" @click="add()">
-              <v-card-text class="text-center mt-1 mb-3">
+            <v-card
+              outlined
+              :elevation="hover ? 1 : 0"
+              class="my-4"
+              @click="add()"
+            >
+              <v-card-text class="text-center py-4">
                 <v-icon size="120" color="grey lighten-1">
                   add_circle_outline
                 </v-icon>
-                <p class="headline mb-0">Agregar nuevo trabajo.</p>
+                <p class="headline mb-1">Agregar nuevo trabajo.</p>
+              </v-card-text>
+            </v-card>
+          </v-hover>
+          <!-- -->
+          <v-hover v-slot:default="{ hover }" open-delay="200">
+            <v-card
+              outlined
+              :elevation="hover ? 1 : 0"
+              class="my-4"
+              @click="add()"
+            >
+              <v-card-text>
+                <!-- -->
+                <v-layout :column="$vuetify.breakpoint.smAndDown">
+                  <v-flex xs12 md4 d-inline-flex>
+                    <v-img
+                      src="/avatars/molly.png"
+                      height="225"
+                      width="225"
+                      aspect-ratio="1"
+                    />
+                  </v-flex>
+                  <v-flex xs12 md8>
+                    <v-layout
+                      column
+                      :pt-2="$vuetify.breakpoint.smAndDown"
+                      :pl-4="$vuetify.breakpoint.mdAndUp"
+                    >
+                      <v-layout align-center fill-height mb-2>
+                        <v-flex xs12 md8>
+                          <v-layout
+                            class="flex-row text-no-wrap"
+                            align-center
+                            fill-height
+                          >
+                            <p class="headline black--text text-truncate mb-0">
+                              Reparación de cañería.
+                            </p>
+                          </v-layout>
+                        </v-flex>
+                        <v-flex xs12 md4>
+                          <v-layout justify-end align-start>
+                            <Rating value="3.5" star />
+                          </v-layout>
+                        </v-flex>
+                      </v-layout>
+                      <v-layout column fill-height align-start>
+                        <p>
+                          Material Design Component Framework Vuetify is a Vue
+                          UI Library with beautifully handcrafted Material
+                          Components. No design skills required — everything you
+                          need to create amazing applications is at your
+                          fingertips.
+                        </p>
+                      </v-layout>
+                    </v-layout>
+                  </v-flex>
+                </v-layout>
+                <!-- -->
               </v-card-text>
             </v-card>
           </v-hover>
