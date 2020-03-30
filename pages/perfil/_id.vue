f620e54028f4eccaac5bd6e2da73876e945e2136
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-03-30 16:00:18 -0300

    Add job card

--- pages/perfil/_id.vue
+++ pages/perfil/_id.vue
@@ -4,8 +4,8 @@
       <v-layout justify-center align-start>
         <v-flex xs12 lg4>
           <v-layout column align-center>
-            <Avatar size="225" :src="perfil.picture" class="ma-2" />
-            <Rating :value="perfil.puntuacion" />
+            <Avatar size="225" :src="perfil.picture" class="ma-2 elevation-3" />
+            <Rating :value="perfil.puntuacion" star />
           </v-layout>
         </v-flex>
         <v-flex xs12 lg8>
@@ -25,8 +25,30 @@
         <v-divider />
       </v-layout>
 
-      <v-layout justify-center>
-        <pre v-text="perfil" />
+      <v-layout mt-4>
+        <p class="headline">Trabajos</p>
+        <v-flex v-show="loadingTrabajos" xs12 class="text-center my-12">
+          <v-progress-circular
+            width="2"
+            indeterminate
+            :active="loadingTrabajos"
+            color="grey darken-1"
+          />
+        </v-flex>
+      </v-layout>
+      <v-layout v-if="!loadingTrabajos" justify-center fill-height mt-0>
+        <v-flex xs12>
+          <v-hover v-slot:default="{ hover }" open-delay="200">
+            <v-card outlined :elevation="hover ? 1 : 0" @click="add()">
+              <v-card-text class="text-center mt-1 mb-3">
+                <v-icon size="120" color="grey lighten-1">
+                  add_circle_outline
+                </v-icon>
+                <p class="headline mb-0">Agregar nuevo trabajo.</p>
+              </v-card-text>
+            </v-card>
+          </v-hover>
+        </v-flex>
       </v-layout>
     </v-flex>
   </v-layout>
@@ -57,6 +79,7 @@ export default {
   },
   data: () => ({
     perfil: {},
+    loadingTrabajos: true,
   }),
   computed: {
     editable() {
@@ -70,6 +93,12 @@ export default {
       )
     },
   },
+  mounted() {
+    const self = this
+    setTimeout(function () {
+      self.loadingTrabajos = false
+    }, 3000)
+  },
   methods: {
     changeBiblo() {
       Persona.save({
@@ -77,6 +106,7 @@ export default {
         bibliography: '<p>I <b>think</b> this is good.</p>',
       })
     },
+    add() {},
   },
 }
 </script>
