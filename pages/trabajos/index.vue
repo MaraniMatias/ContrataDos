4df08a2e66eb73f0ce377ddb746d32c97e17228d
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-03-29 23:35:57 -0300

    Show tutorial

--- pages/trabajos/index.vue
+++ pages/trabajos/index.vue
@@ -1,10 +1,69 @@
 <template>
-  <v-layout>
-    <v-flex class="text-center">
-      Ir a prefil y actualizar
-      <v-btn @click="testApi">Test api</v-btn>
-      <pre v-text="data" />
+  <v-layout column justify-center pb-12 mb-2 px-2>
+    <v-flex v-if="user.show_tutorial" xs12 sm12 md10 lg10 xl6 mt-4 mb-2>
+      <v-card class="mx-auto" max-width="344" outlined>
+        <v-list-item three-line>
+          <v-list-item-content>
+            <v-list-item-title class="headline mb-1">
+              Bienvenido
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              <p>
+                Eres nuevo, deberías completar tus perfil y contarnos de tus
+                trabajos terminados.
+              </p>
+              <p>
+                Para ayudar a que otros encuentre tus trabajo y tus servicios,
+                te recomendamos completar tu perfil.
+              </p>
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn text to="/perfil" color="primary">Ir a tú perfil</v-btn>
+        </v-card-actions>
+      </v-card>
     </v-flex>
+
+    <template v-else>
+      <v-flex xs12 sm12 md10 lg10 xl6 class="text-center mb-2">
+        <span>Resultados de la búsqueda: {{ totalElement }}</span>
+      </v-flex>
+
+      <v-flex v-show="loadingApunte" xs12 md10 class="text-center my-12">
+        <v-progress-circular
+          width="2"
+          indeterminate
+          :active="loadingApunte"
+          color="grey darken-1"
+        />
+        <p class="mt-6">Buscando...</p>
+      </v-flex>
+
+      <v-layout justify-center fill-height mt-0>
+        <v-flex xs12 md11 lg10 xl6>
+          <!--
+        <Documento
+          :doc="doc"
+          :key="i"
+          v-for="(doc, i) in items"
+          @loadingPreview="loadingPreview"
+          @snackbar="showMsg"
+        />
+        <infinite-loading
+          spinner="spiral"
+          @infinite="infiniteHandler"
+        >
+          <template v-slot:no-more>Eso es todo.</template>
+          <template v-slot:no-results>
+            <span>Eso es todo.</span>
+          </template>
+        </infinite-loading>
+        -->
+        </v-flex>
+      </v-layout>
+    </template>
   </v-layout>
 </template>
 
@@ -12,13 +71,13 @@
 export default {
   middleware: 'authenticated',
   data: () => ({
-    data: {},
+    totalElement: 0,
   }),
-  methods: {
-    async testApi() {
-      const { data } = await this.$http.get('http://localhost:3000/api/persona')
-      this.data = data
+  computed: {
+    user() {
+      return this.$store.state.user || {}
     },
   },
+  methods: {},
 }
 </script>
