4da17de6229e6a8196e12ebc659f6295440cb343
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-03-30 00:50:29 -0300

    Using api base

--- pages/perfil/_id.vue
+++ pages/perfil/_id.vue
@@ -1,9 +1,34 @@
 <template>
-  <v-layout> </v-layout>
+  <v-layout>
+    <pre v-text="perfil" />
+  </v-layout>
 </template>
 
 <script>
+import api from '~/api/baseApi'
+const Persona = api('/Persona')
+
 export default {
+  // middleware: 'authenticated', es publico
   components: {},
+  validate({ params }) {
+    if (params.id) {
+      return /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i.test(params.id)
+    } else {
+      return true
+    }
+  },
+  async asyncData({ params, store }) {
+    if (params.id) {
+      const { data: perfil } = await Persona.getById(params.id)
+      return { perfil }
+    } else {
+      return { perfil: store.state.user }
+    }
+  },
+  data: () => ({
+    perfil: {},
+  }),
+  methods: {},
 }
 </script>
