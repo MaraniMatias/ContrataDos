4519f35477bd35ef768fd84d00a47cc4a0726e6c
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-03-30 00:49:50 -0300

    Make api base

---
+++ api/baseApi.js
@@ -0,0 +1,44 @@
+import http from './http'
+
+const getAll = (BASE_URL) => async () => {
+  try {
+    return await http.get(BASE_URL)
+  } catch (err) {
+    return { error: err.message || 'ApiError' }
+  }
+}
+
+const getById = (BASE_URL) => async (_id) => {
+  try {
+    return await http.get(BASE_URL + `/${_id}`)
+  } catch (err) {
+    return { error: err.message || 'ApiError' }
+  }
+}
+
+const save = (BASE_URL) => async (object = {}) => {
+  try {
+    if (object._id) return await http.post(BASE_URL, object)
+    else return await http.path(BASE_URL + `/${object._id}`, object)
+  } catch (err) {
+    return { error: err.message || 'ApiError' }
+  }
+}
+
+const del = (BASE_URL) => async (object = {}) => {
+  try {
+    return await http.path(BASE_URL + `/${object._id}`, { deleted: true })
+  } catch (err) {
+    return { error: err.message || 'ApiError' }
+  }
+}
+
+export default (url) => {
+  const BASE_URL = process.env.BASE_URL + '/api' + url
+  return {
+    save: save(BASE_URL),
+    delete: del(BASE_URL),
+    getAll: getAll(BASE_URL),
+    getById: getById(BASE_URL),
+  }
+}
