bd741e0c409362cfc0d91a758f397e7cab0f1b79
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-03-30 21:13:24 -0300

    Add crop image component

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -1,32 +1,75 @@
 <template>
-  <v-avatar :size="size" style="cursor: pointer;" v-on="{ ...on }">
-    <img
-      v-if="!avatarError && !!src"
+  <v-hover v-slot:default="{ hover }">
+    <v-avatar
+      :size="size"
+      :class="{ cursor: editable || hover }"
+      color="grey"
+      v-on="{ ...on }"
+    >
+      <v-icon v-if="hover" size="96" color="white">camera_alt</v-icon>
+      <template v-else>
+        <img
+          v-if="!avatarError && !!src"
+          :src="src"
+          @error.stop="avatarError = true"
+        />
+        <img v-else src="~/assets/images/user_default.png" />
+      </template>
+    </v-avatar>
+  </v-hover>
+  <!--
+  <div>
+    <cropper
       :src="src"
-      @error.stop="avatarError = true"
+      stencil-component="circle-stencil"
+      :style="style"
+      @change="change"
     />
-    <img v-else src="~/assets/images/user_default.png" />
-  </v-avatar>
+  </div>
+  -->
 </template>
 
 <script>
+// import { Cropper } from 'vue-advanced-cropper'
+
 export default {
+  // components: { Cropper },
   props: {
     size: { type: String, default: '32' },
     src: { type: String, default: '' },
     on: { type: Object, default: () => {} },
+    editable: { type: Boolean, default: false },
   },
   data: () => ({
     avatarError: false,
+    modal: false,
   }),
-  methods: {},
+  computed: {
+    style() {
+      return {
+        height: this.size + 'px',
+        background: '#ddd',
+      }
+    },
+  },
+  watch: {},
+  methods: {
+    showModalEdit() {
+      this.modal = true
+    },
+    change({ coordinates }) {
+      console.log(coordinates)
+    },
+  },
 }
 </script>
->
 
 <style scoped>
 .logo {
   margin: 2px 0 0 0;
   height: 42px;
+}
+.cursor {
+  cursor: pointer;
 }
 </style>
---
+++ components/DateField.vue
@@ -0,0 +1,140 @@
+<template>
+  <v-menu
+    ref="menu"
+    v-model="menu"
+    :close-on-content-click="false"
+    max-width="290px"
+    min-width="290px"
+    offset-y
+    transition="scale-transition"
+  >
+    <template v-slot:activator="{ on }">
+      <v-text-field
+        v-model="input"
+        :index="index"
+        :label="label"
+        :hide-details="hideDetails"
+        :clearable="clearable"
+        prepend-icon="event"
+        :dense="dense"
+        readonly
+        :rules="rule"
+        @click:clear="clear"
+        v-on="on"
+      />
+    </template>
+    <v-date-picker
+      ref="picker"
+      v-model="parseDate"
+      locale="es"
+      :min="min"
+      :max="max"
+      @change="saveDate"
+    />
+  </v-menu>
+</template>
+
+<script>
+import parseDateFormat from '../utils/dateFormat'
+import formRules from '../utils/formRules'
+
+// :max="new Date().toISOString().substr(0, 10)"
+// :min="new Date().toISOString().substr(0, 10)"
+
+export default {
+  props: {
+    label: String,
+    hideDetails: Boolean,
+    value: [Date, String],
+    index: [Number, String],
+    clearable: { type: Boolean, default: false },
+    onlyAdult: { type: Boolean, default: false },
+    required: { type: Boolean, default: false },
+    dense: { type: Boolean, default: false },
+    startOnYear: { type: Boolean, default: false },
+    min: String,
+    max: String,
+  },
+  data: () => ({
+    date: '',
+    menu: '',
+  }),
+  computed: {
+    formRules: () => formRules,
+    rule() {
+      const rules = []
+      if (this.required) rules.push(this.formRules.required())
+      if (this.onlyAdult) rules.push(this.isAdult)
+      return rules
+    },
+    isAdult() {
+      if (this.date instanceof Date) {
+        return (
+          new Date().setFullYear(new Date().getFullYear() - 18) >
+            this.date.getTime() ||
+          'Para usar Rapicopia tienes que tener más de 18 años.'
+        )
+      } else {
+        return false
+      }
+    },
+    parseDate: {
+      get() {
+        const parseDate = this.value
+        if (!parseDate) return null
+        return new Date(parseDate).toISOString().substr(0, 10)
+      },
+      set(val) {
+        if (val) {
+          const [year, month, day] = val
+            .replace(/T\d+:\d+:\d+.\d+\+\d+$/, '')
+            .split('-')
+          const parseDate = new Date()
+          parseDate.setFullYear(year)
+          parseDate.setMonth(month - 1)
+          parseDate.setDate(day)
+          parseDate.setHours(0, 0, 0, 0)
+          this.date = parseDate
+        }
+      },
+    },
+    input: {
+      get() {
+        const parseDate = this.value
+        if (!parseDate) return null
+        return parseDateFormat(parseDate, 'dd/MM/yyyy')
+      },
+      set(val) {
+        if (val) this.date = new Date(val)
+        else this.date = null
+      },
+    },
+  },
+  watch: {
+    menu(val) {
+      if (this.startOnYear) {
+        val &&
+          !this.date &&
+          setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'), 100)
+      }
+    },
+    date(val) {
+      this.$emit('input', val)
+    },
+  },
+  created() {
+    this.data = this.value
+  },
+  mounted() {
+    this.$refs.menu.save(this.value)
+  },
+  methods: {
+    saveDate(parseDate) {
+      this.$refs.menu.save(parseDate)
+    },
+    clear() {
+      this.$emit('input', null)
+    },
+  },
+}
+</script>
