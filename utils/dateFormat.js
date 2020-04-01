54fae9f5e19f322a90348f61773f70d1968faf05
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-03-31 22:50:35 -0300

    untracked files on master: e77f923 Using modal to change perfil data

---
+++ utils/dateFormat.js
@@ -0,0 +1,11 @@
+// https://date-fns.org/v2.0.1/docs/I18n
+import format from "date-fns/format";
+import es from "date-fns/locale/es";
+const locales = { es };
+
+export default function (value, formatStr = "PP") {
+  const date = typeof value === "string" ? new Date(value) : value;
+  return format(date, formatStr, {
+    locale: locales[window.__localeId__], // or global.__localeId__
+  });
+}
