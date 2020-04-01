963772e14616ccdc7455e00f19402f45efbda90a
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-04-01 19:52:13 -0300

    Add router to save trabajo and image

--- server/models/trabajo.js
+++ server/models/trabajo.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const escapeHtml = require('escape-html')
 const Schema = mongoose.Schema
 const ObjectId = mongoose.Schema.Types.ObjectId
 
@@ -13,17 +14,21 @@ const schema = new Schema(
     cliente: { type: ObjectId, ref: 'persona' },
     profesional: { type: ObjectId, ref: 'persona' },
     localidad: { type: ObjectId, ref: 'localidad' },
-    tipo: [
-      {
-        type: String,
-        default: TipoTrabajo.PUBLICO, // Son privados los que son programados/pendientres, o finalizados pero no publicados
-        enum: [TipoTrabajo.PRIVADO, TipoTrabajo.PRIVADO],
-      },
-    ],
+    tipo: {
+      type: String,
+      default: TipoTrabajo.PUBLICO, // Son privados los que son programados/pendientres, o finalizados pero no publicados
+      enum: [TipoTrabajo.PRIVADO, TipoTrabajo.PRIVADO],
+    },
     puntuacion: { type: Number, default: 0 },
     // foto, usar el _id para referenciar la foto o mejor un id unico universal
-    descripcion: String,
-    descripcion_breve: { type: String, trim: true, required: true, max: 120 },
+    descripcion: { type: String, max: 500, trim: true, set: escapeHtml },
+    descripcion_breve: {
+      type: String,
+      trim: true,
+      required: true,
+      max: 120,
+      set: escapeHtml,
+    },
     // servicios: [{ type: ObjectId, ref: 'habilidad' }],
     servicios: [{ type: String }],
     agenda: [{ type: ObjectId, ref: 'agenda' }], // Lista de eventos, agenda, si se re programa tiene varios
--- server/router/file.js
+++ server/router/file.js
@@ -2,10 +2,33 @@ import { sendRes, auth } from '../utilities/router'
 // import { checkErrors, check } from '../utilities/checkProps'
 import { save } from '../utilities/fileImagen'
 import { Persona } from '../models/persona'
+import { Trabajo } from '../models/trabajo'
 import router from './nuxtRouter'
 
 const multer = require('multer')
 const upload = multer()
+
+router.post('/trabajo', auth.isLogin, upload.single('file'), async function (
+  req,
+  res
+) {
+  try {
+    // TODO falta los check del body
+    const model = JSON.parse(req.body.dto)
+    model.profesional = req.user._id
+    model.deleted = false
+
+    const trabajo = await Trabajo.create(model)
+    const _id = trabajo._id
+    const extension = req.file.originalname.match(/[^.]+$/)[0]
+    const fileName = _id + '.' + extension
+    await save(req.file.buffer, '/jobs/' + fileName)
+
+    return sendRes(res, 200, trabajo, 'Success', null)
+  } catch (err) {
+    return sendRes(res, 500, null, err ? err.message : 'Algo salio mal :(', err)
+  }
+})
 
 router.post('/perfil', auth.isLogin, upload.single('file'), async function (
   req,
@@ -14,21 +37,17 @@ router.post('/perfil', auth.isLogin, upload.single('file'), async function (
   try {
     // TODO falta los check del body
     const _id = req.user._id
+
     const extension = req.file.originalname.match(/[^.]+$/)[0]
     const fileName = _id + '.' + extension
     await save(req.file.buffer, '/perfil/' + fileName)
+
     const picturePath = '/images/perfil/' + fileName
-    await Persona.updateOne({ _id }, { picture: picturePath })
+    await Persona.findByIdAndUpdate(_id, { picture: picturePath })
     req.user.picture = picturePath
     return sendRes(res, 200, req.user, 'Success', null)
   } catch (err) {
-    return sendRes(
-      res,
-      500,
-      null,
-      'error',
-      err ? err.message : 'Algo salio mal :('
-    )
+    return sendRes(res, 500, null, err ? err.message : 'Algo salio mal :(', err)
   }
 })
 
@@ -36,18 +55,9 @@ router.post('/perfil', auth.isLogin, upload.single('file'), async function (
 router.get('/perfil', async function (req, res) {
   try {
     const fileName = req.query.nombre + '.' + req.query.extension
-    return res.download(
-      path.join(DOC_BASE_PATH, req.params.uuid + '.' + req.query.extension),
-      fileName
-    )
+    return res.download(path.join(DOC_BASE_PATH, req.params.uuid + '.' + req.query.extension),fileName)
   } catch (err) {
-    return sendRes(
-      res,
-      500,
-      null,
-      'error',
-      err ? err.message : 'Algo salio mal :('
-    )
+    return sendRes(res,500,null,'error',err ? err.message : 'Algo salio mal :(')
   }
 })
 */
