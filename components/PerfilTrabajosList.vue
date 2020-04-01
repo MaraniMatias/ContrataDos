9893a6fbc2111372e0f3de23945517174b0786c7
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-04-01 19:52:44 -0300

    Add save trabajo modal

--- components/PerfilTrabajosList.vue
+++ components/PerfilTrabajosList.vue
@@ -4,7 +4,7 @@
       <v-flex>
         <p class="headline mb-0">Trabajos</p>
       </v-flex>
-      <v-btn v-if="showAddBtn" icon text @click="add">
+      <v-btn v-if="showAddBtn" icon text @click.stop="showModal = true">
         <v-icon>add</v-icon>
       </v-btn>
     </v-layout>
@@ -22,42 +22,162 @@
 
     <v-layout v-if="!loadingTrabajos" column>
       <v-flex v-if="listTrabajos.length === 0" xs12>
-        <CardTrabajoAdd @click="add" />
+        <CardTrabajoAdd @click.stop="showModal = true" />
       </v-flex>
-      <v-flex xs12>
-        <CardTrabajo publish />
-      </v-flex>
-      <v-flex xs12>
-        <CardTrabajo />
+      <v-flex v-for="(trabajo, $i) in listTrabajos" :key="$i" xs12>
+        <CardTrabajo :trabajo="trabajo" />
       </v-flex>
     </v-layout>
+    <v-dialog v-model="showModal" width="550" persistent @keyup.esc="close">
+      <CardForm @submit="saveTrabajo">
+        <template v-slot:header>Agregar Trabajo</template>
+        <template v-slot:default="{ rules }">
+          <v-flex xs12 mx-2>
+            <v-text-field
+              v-model.lazy="form.descripcion_breve"
+              dense
+              hide-details
+              label="Titulo o Descripción breve"
+              outlined
+              :readonly="loading"
+              counter
+              :rules="[rules.required(), rules.max(120)]"
+            />
+          </v-flex>
+          <v-flex xs12 ma-2>
+            <v-select
+              v-model.lazy="form.localidad"
+              dense
+              hide-details
+              :items="localidades"
+              item-text="nombre"
+              item-value="_id"
+              label="Localidad"
+              outlined
+              return-object
+              :readonly="loading"
+              :rules="[rules.required()]"
+            />
+          </v-flex>
+          <v-flex xs12 ma-2>
+            <v-textarea
+              v-model.lazy="form.descripcion"
+              auto-grow
+              counter="500"
+              dense
+              label="Descripción"
+              outlined
+              :readonly="loading"
+              :rules="[rules.max(500)]"
+            />
+          </v-flex>
+          <v-flex xs12 ma-2>
+            <v-layout align-center>
+              <v-btn
+                depressed
+                color="secondary"
+                :disabled="loading"
+                @click.stop="pickupImg = true"
+              >
+                Elegir foto
+              </v-btn>
+              <p class="mb-0 mx-4" v-text="form.fileName" />
+              <CardCropper :on.sync="pickupImg" @submit="saveTrabajoImg" />
+            </v-layout>
+          </v-flex>
+        </template>
+        <!--
+        <template v-slot:message>
+          <error :text="error" />
+        </template>
+        -->
+        <template v-slot:actions>
+          <v-btn text :disabled="loading" @click="close">Cancelar </v-btn>
+          <v-btn :disabled="loading" color="primary" type="submit">
+            Guardar
+          </v-btn>
+        </template>
+      </CardForm>
+    </v-dialog>
   </v-layout>
 </template>
 
 <script>
 import CardTrabajoAdd from '~/components/CardTrabajoAdd'
 import CardTrabajo from '~/components/CardTrabajo'
+import CardForm from '~/components/CardForm'
+import CardCropper from '~/components/CardCropper'
+
+import apiFile from '~/api/file'
+import api from '~/api/baseApi'
+const onFileUpload = 'progressFileUpload'
+const { trabajo: saveImg } = apiFile(onFileUpload)
+const Trabajo = api('/Trabajo')
+const Localidad = api('/Localidad')
 
 export default {
-  components: { CardTrabajo, CardTrabajoAdd },
+  components: { CardTrabajo, CardTrabajoAdd, CardForm, CardCropper },
   props: {
     showAddBtn: { type: Boolean, default: false },
     profilId: { type: String, required: true },
   },
   data: () => ({
+    loading: false,
     loadingTrabajos: true,
     listTrabajos: [],
+    pickupImg: false,
+    showModal: false,
+    localidades: [],
+    form: {
+      fileName: '',
+      localidad: {},
+      descripcion_breve: '',
+      descripcion: '',
+    },
+    pickedFile: null,
   }),
   computed: {},
-  mounted() {
-    const self = this
-    setTimeout(function () {
-      self.loadingTrabajos = false
-    }, 3000)
+  async mounted() {
+    this.getTrabajos()
+
+    // TODO search query
+    const { data } = await Localidad.getAll()
+    this.localidades = data || []
   },
   methods: {
-    add() {
-      console.log('Add')
+    async getTrabajos() {
+      this.loadingTrabajos = true
+      // get los del perfil
+      const { data } = await Trabajo.getAll()
+      this.listTrabajos = data || []
+      this.loadingTrabajos = false
+    },
+    saveTrabajoImg({ file, fileName }) {
+      this.pickedFile = file
+      this.form.fileName = fileName
+      this.pickupImg = false
+    },
+    async saveTrabajo() {
+      this.loading = true
+      const { error } = await saveImg(this.pickedFile, this.form)
+      if (error) {
+        this.$notify({ type: 'error', text: 'Error al subir la foto.' })
+      } else {
+        this.close()
+        this.getTrabajos()
+      }
+      this.loading = false
+    },
+    close() {
+      this.showModal = false
+      this.pickedFile = null
+      this.pickupImg = false
+      this.form = {
+        fileName: '',
+        localidad: {},
+        descripcion_breve: '',
+        descripcion: '',
+      }
     },
   },
 }
