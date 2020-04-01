15e3316f0597aa15f615abc710b60620c833e777
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-04-01 19:53:50 -0300

    Add bytes filter

---
+++ utils/bytes.js
@@ -0,0 +1,12 @@
+const bytesFilter = (bytes = 0, precision = 2) => {
+  if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-'
+  const units = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+  const number = bytes === 0 ? 0 : Math.floor(Math.log(bytes) / Math.log(1024))
+  return (
+    (bytes / 1024 ** Math.floor(number)).toFixed(precision) +
+    ' ' +
+    units[number]
+  )
+}
+
+export default bytesFilter
