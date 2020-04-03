ceac62d6d554d31dcfb2e7760f4622dd33b2a656
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-04-03 00:04:24 -0300

    Search professional

--- pages/index.vue
+++ pages/index.vue
@@ -10,19 +10,41 @@
         style="margin: 2px 0 0 0; height: 160px;"
       />
       <v-col cols="12">
-        <v-text-field
+        <v-autocomplete
           ref="search"
-          v-model.lazy="query"
-          outlined
+          v-model="select"
+          append-icon=""
           border-radius
+          cache-items
+          flat
           hide-details
-          append-icon="search"
-          @keyup.enter="search()"
-          @click:append="search()"
-        />
+          hide-no-data
+          :items="items"
+          item-text="text"
+          :loading="loading"
+          menu-props="closeOnContentClick"
+          multiple
+          outlined
+          return-object
+          @keyup.enter="btnSearch()"
+          @click:append="btnSearch()"
+        >
+          <template v-slot:selection="data">
+            <v-chip
+              close
+              :input-value="data.selected"
+              @click="data.select"
+              @click:close="remove(data.item)"
+              v-text="data.item.text"
+            />
+          </template>
+          <template v-slot:item="data">
+            {{ data.item.text }}
+          </template>
+        </v-autocomplete>
       </v-col>
       <v-col cols="12">
-        <v-btn rounded color="primary" x-large @click="search">
+        <v-btn rounded color="primary" x-large @click="btnSearch">
           Buscar profesional
         </v-btn>
       </v-col>
@@ -31,16 +53,59 @@
 </template>
 
 <script>
+import { Localidad, Habilidad } from '~/api'
+
 export default {
   components: {},
+  async asyncData() {
+    let items = []
+    const { data: localidades } = await Localidad.getAll()
+    const localidadList = localidades.map(({ _id, nombre }) => ({
+      _id,
+      text: nombre,
+      type: 'localidad',
+    }))
+    items = items.concat(localidadList)
+
+    const { data: habilidades } = await Habilidad.getAll()
+    const habilidadList = habilidades.map(({ _id, nombre }) => ({
+      _id,
+      text: nombre,
+      type: 'habilidad',
+    }))
+    items = items.concat(habilidadList)
+
+    return { items }
+  },
   data: () => ({
-    query: '',
+    loading: false,
+    items: [],
+    search: null,
+    select: [],
   }),
   computed: {},
+  /* watch: {
+    search(val) {
+      if (this.loading === false) {
+        val && val.length > 2 && this.querySelections(val)
+      }
+    },
+  }, */
   methods: {
-    search() {
-      if (this.query) {
-        this.$router.replace({ name: 'search', query: { q: this.query } })
+    makeQuery(type) {
+      return this.select
+        .filter((item) => item.type === type)
+        .map(({ _id }) => _id)
+    },
+    btnSearch() {
+      if (this.select.length) {
+        this.$router.replace({
+          name: 'search',
+          query: {
+            profesion: this.makeQuery('habilidad'),
+            localidad: this.makeQuery('localidad'),
+          },
+        })
       } else {
         const self = this
         this.$nextTick(function () {
@@ -48,6 +113,14 @@ export default {
         })
       }
     },
+    /* querySelections(q) {
+      this.loading = true
+      this.items = []
+      this.$http.get('/api/search', { params: { q } }).then(({ data }) => {
+        this.items = data
+        this.loading = false
+      })
+    }, */
   },
 }
 </script>
--- pages/search.vue
+++ pages/search.vue
@@ -1,18 +1,57 @@
 <template>
-  <v-layout>
-    <v-flex xs12 sm12 md10 lg10 xl6 class="text-center mb-2">
-      <span>Resultados de la búsqueda: {{ totalElement }}</span>
+  <v-layout row pb-12 mb-2 px-2 justify-center>
+    <v-flex xs12 sm12 md10 lg8 xl6 mt-4 mb-2>
+      <v-layout column justify-start>
+        <v-flex xs12 sm12 md10 lg10 xl6 class="text-center mb-2">
+          <span>Resultados de la búsqueda: {{ totalItems }}</span>
+        </v-flex>
+        <v-layout v-show="loading" justify-center mt-3>
+          <v-flex xs12 class="text-center">
+            <v-progress-circular
+              width="2"
+              indeterminate
+              active
+              color="grey darken-1"
+            />
+          </v-flex>
+        </v-layout>
+        <pre>{{ items }}</pre>
+      </v-layout>
     </v-flex>
-
-    {{ $route.query.q }}
   </v-layout>
 </template>
 
 <script>
+import { Persona } from '~/api'
+
 export default {
   components: {},
+  asyncData({ query, redirect, error }) {
+    // error({ message: e.message || 'Internal error', statusCode: 500 })
+    if (
+      typeof query.profesion === 'undefined' &&
+      typeof query.localidad === 'undefined'
+    ) {
+      redirect('/')
+    }
+  },
   data: () => ({
-    totalElement: 0,
+    loading: true,
+    items: [],
+    totalItems: 0,
   }),
+  async mounted() {
+    const query = {}
+    const servicios = this.$route?.query?.profesion
+    const localidad = this.$route?.query?.localidad
+    if (servicios?.length) query.servicios = servicios
+    if (localidad?.length) query.localidad = localidad
+    const { data, totalItems } = await Persona.get({ query })
+    this.items = data
+    this.totalItems = totalItems || 0
+
+    this.loading = false
+  },
+  methods: {},
 }
 </script>
