e3c6de2523f1311ff0fd53cfa50bb0a520e9f747
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-04-03 21:33:53 -0300

    Save tutorial

--- pages/trabajos/index.vue
+++ pages/trabajos/index.vue
@@ -1,50 +1,22 @@
 <template>
-  <v-layout column pb-12 mb-2 px-2 align-center>
-    <v-flex v-if="showTutorial" xs12 sm12 md10 lg10 xl6 mt-4 mb-2>
-      <v-card class="mx-auto" max-width="344" outlined>
-        <v-list-item three-line>
-          <v-list-item-content>
-            <v-list-item-title class="headline mb-1">
-              Bienvenido
-            </v-list-item-title>
-            <v-list-item-subtitle>
-              <p>
-                Eres nuevo, deberías completar tus perfil y contarnos de tus
-                trabajos terminados.
-              </p>
-              <p>
-                Para ayudar a que otros encuentre tus trabajo y tus servicios,
-                te recomendamos completar tu perfil.
-              </p>
-            </v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn text to="/perfil" color="primary">Ir a tú perfil</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-    <template v-else>
-      <v-layout fill-height>
-        <v-flex>
-          <p class="headline mb-0">Trabajos Pendientes</p>
+  <v-layout pb-12 mb-2 px-2 fill-height>
+    <v-layout column>
+      <v-flex xs12 sm12 md10 lg8 xl6 mt-4 mb-2>
+        <p class="headline mb-0">Trabajos Pendientes</p>
+
+        <v-flex v-show="loadingTrabajos" x12 mt-4 mb-2 class="text-center">
+          <v-progress-circular
+            width="2"
+            indeterminate
+            :active="loadingTrabajos"
+            color="grey darken-1"
+          />
+          <p class="mt-6">Buscando...</p>
         </v-flex>
-      </v-layout>
 
-      <v-flex v-show="loadingTrabajos" xs12 md10 class="text-center my-12">
-        <v-progress-circular
-          width="2"
-          indeterminate
-          :active="loadingTrabajos"
-          color="grey darken-1"
-        />
-        <p class="mt-6">Buscando...</p>
-      </v-flex>
-
-      <v-layout justify-center fill-height mt-0>
-        <v-flex xs12 md11 lg10 xl6>
-          <!--
+        <v-layout justify-center mt-0>
+          <v-flex xs12 md11 lg10 xl6>
+            <!--
         <Documento
           :doc="doc"
           :key="i"
@@ -62,13 +34,44 @@
           </template>
         </infinite-loading>
         -->
-        </v-flex>
-      </v-layout>
-    </template>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+
+    <v-dialog v-model="showTutorial" width="550">
+      <v-card>
+        <v-card-title>
+          <p class="headline">Bienvenido</p>
+        </v-card-title>
+        <v-card-text>
+          <p>
+            Eres nuevo, deberías completar tus perfil y contarnos de tus
+            trabajos terminados.
+          </p>
+          <v-divider />
+          <p class="title my-2">Soy un trabajador</p>
+          <p>
+            Para ayudar a que otros encuentre tus trabajo y tus servicios, te
+            recomendamos completar tu perfil.
+            <a nuxt href="/perfil" class="black--text"> Ir a tú perfil </a>
+          </p>
+          <v-layout justify-end align-center>
+            <v-btn text color="primary" @click="close">
+              Aceptar
+            </v-btn>
+          </v-layout>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
   </v-layout>
 </template>
 
 <script>
+import { mapMutations } from 'vuex'
+
+import { Persona } from '~/api'
+
 export default {
   middleware: 'authenticated',
   data: () => ({
@@ -80,9 +83,18 @@ export default {
       return this.$store.state.user || {}
     },
     showTutorial() {
-      return this.user?.['show_tutorial'] || false
+      return this.user?.['show_tutorial'] ?? true
     },
   },
-  methods: {},
+  methods: {
+    ...mapMutations({ updateUser: 'SET_USER' }),
+    async close() {
+      const { data } = await Persona.save({
+        _id: this.user._id,
+        show_tutorial: false,
+      })
+      if (data) this.updateUser(data)
+    },
+  },
 }
 </script>
