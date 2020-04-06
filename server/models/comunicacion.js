88860f8dbc42920f3a6d88d39a694a06e27565df
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-04-06 15:53:51 -0300

    Add set date

--- server/models/comunicacion.js
+++ server/models/comunicacion.js
@@ -7,7 +7,8 @@ const schema = new Schema(
     from: { type: ObjectId, required: true, ref: 'persona' },
     to: { type: ObjectId, required: true, ref: 'persona' },
     trabajo: { type: ObjectId, required: true, ref: 'trabajo' },
-    detalle: { type: String, trim: true, required: true, max: 500 },
+    detalle: { type: String, trim: true, max: 120 },
+    fecha: { type: Date }, // Propone una fecha para el trabajo
   },
   { timestamps: true }
 )
