b13afc42e1d9071e2607cda4dd289f7e575ec59b
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-04-10 18:09:29 -0300

    Add task name, fix connections, and add define

--- server/utilities/agendaTask.js
+++ server/utilities/agendaTask.js
@@ -1,18 +1,21 @@
+const mongoose = require('mongoose')
 const Agenda = require('agenda')
-const getMongoURL = require('./getMongoURL')
 const sendEmailTo = require('./sendEmail')
 let agenda
 
 // Tasks
-module.exports.jobName = {
-  SEND_EMAIL_ANULADA: 'SEND_EMAIL_ANULADA',
-  SEND_EMAIL_A_VERIFICAR: 'SEND_EMAIL_A_VERIFICAR',
-  SEND_EMAIL_EDIT_USER: 'SEND_EMAIL_EDIT_USER',
-  SEND_EMAIL_MEW_USER: 'SEND_EMAIL_MEW_USER',
-  SEND_EMAIL_RECHAZADA: 'SEND_EMAIL_RECHAZADA',
-  SEND_EMAIL_REPROGRAMACION: 'SEND_EMAIL_REPROGRAMACION',
-  SEND_EMAIL_SOLICITADA: 'SEND_EMAIL_SOLICITADA',
-  SEND_EMAIL_VERIFICADA: 'SEND_EMAIL_VERIFICADA',
+module.exports.jobs = {
+  SEND_EMAIL: {
+    VERIFICAR_EAMIL: 'SEND_EMAIL_VERIFICAR_EAMIL',
+    NUEVA_CONSULTA: 'NUEVA_CONSULTA',
+    NUEVO_MENSAJE: 'NUEVO_MENSAJE',
+    CITA_ACEPTADA: 'CITA_ACEPTADA',
+    CLASIFICAR_TRABAJO: 'CLASIFICAR_TRABAJO',
+    TRABAJO_CANCELADO: 'TRABAJO_CANCELADO',
+  },
+  AUTOMATICA: {
+    TRABAJOS_PARA_HOY: 'TRABAJOS_PARA_HOY',
+  },
 }
 
 module.exports.jobCreate = (jobName, data) =>
@@ -20,133 +23,15 @@ module.exports.jobCreate = (jobName, data) =>
 
 module.exports.start = async function () {
   try {
-    agenda = new Agenda({ db: { address: getMongoURL() } })
+    agenda = new Agenda({ mongo: mongoose.connection })
 
     agenda.define(
-      this.jobName.SEND_EMAIL_MEW_USER,
+      this.jobs.SEND_EMAIL.VERIFICAR_EAMIL,
       { priority: 'normal', concurrency: 2 },
       async (job) => {
         await sendEmailTo(
-          { type: 'user_abm', subject: 'Municipio Verde - Nuevo Usuario' },
+          { subject: 'Bienvenido', template: 'new_user.pug' },
           job.attrs.data
-        )
-      }
-    )
-
-    agenda.define(
-      this.jobName.SEND_EMAIL_EDIT_USER,
-      { priority: 'normal', concurrency: 2 },
-      async (job) => {
-        await sendEmailTo(
-          {
-            type: 'user_abm',
-            subject: 'Municipio Verde - Modificación de datos de acceso',
-          },
-          job.attrs.data
-        )
-      }
-    )
-
-    agenda.define(
-      this.jobName.SEND_EMAIL_SOLICITADA,
-      { priority: 'normal', concurrency: 2 },
-      async (job) => {
-        const data = job.attrs.data
-        await sendEmailTo(
-          {
-            type: 'aplicacion_change_solicitada',
-            subject:
-              'Municipio Verde - SOLICITADA - ' +
-              data.fecha_realizacion +
-              `hs - Lote: ${data.lote}`,
-          },
-          data
-        )
-      }
-    )
-    agenda.define(
-      this.jobName.SEND_EMAIL_A_VERIFICAR,
-      { priority: 'normal', concurrency: 2 },
-      async (job) => {
-        const data = job.attrs.data
-        await sendEmailTo(
-          {
-            type: 'aplicacion_change_a_verificar',
-            subject:
-              'Municipio Verde - VERIFICAR - ' +
-              data.fecha_realizacion +
-              `hs - Lote: ${data.lote}`,
-          },
-          data
-        )
-      }
-    )
-    agenda.define(
-      this.jobName.SEND_EMAIL_REPROGRAMACION,
-      { priority: 'normal', concurrency: 2 },
-      async (job) => {
-        const data = job.attrs.data
-        await sendEmailTo(
-          {
-            type: 'aplicacion_change_reprogramacion',
-            subject:
-              // "Municipio Verde - REPROGRAMACIÓN - " +
-              'Municipio Verde - MODIFICACIÓN - ' +
-              data.fecha_realizacion +
-              `hs - Lote: ${data.lote}`,
-          },
-          data
-        )
-      }
-    )
-    agenda.define(
-      this.jobName.SEND_EMAIL_ANULADA,
-      { priority: 'normal', concurrency: 2 },
-      async (job) => {
-        const data = job.attrs.data
-        await sendEmailTo(
-          {
-            type: 'aplicacion_change_anulada',
-            subject:
-              'Municipio Verde - ANULACIÓN - ' +
-              data.fecha_realizacion +
-              `hs - Lote: ${data.lote}`,
-          },
-          data
-        )
-      }
-    )
-    agenda.define(
-      this.jobName.SEND_EMAIL_RECHAZADA,
-      { priority: 'normal', concurrency: 2 },
-      async (job) => {
-        const data = job.attrs.data
-        await sendEmailTo(
-          {
-            type: 'aplicacion_change_rechazada',
-            subject:
-              'Municipio Verde - RECHAZO - ' +
-              data.fecha_realizacion +
-              `hs - Lote: ${data.lote}`,
-          },
-          data
-        )
-      }
-    )
-    agenda.define(
-      this.jobName.SEND_EMAIL_VERIFICADA,
-      { priority: 'normal', concurrency: 2 },
-      async (job) => {
-        const data = job.attrs.data
-        await sendEmailTo(
-          {
-            type: 'aplicacion_change_verificada',
-            subject:
-              'Municipio Verde - VERIFICADA - ' +
-              data.fecha_realizacion +
-              `hs - Lote: ${data.lote}`,
-          },
-          data
         )
       }
     )
