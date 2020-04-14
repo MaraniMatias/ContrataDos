bbb4ebb586fb1e4d576e0bbc99a0f4981c335776
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-04-13 22:59:03 -0300

    Send email for new message

--- server/router/comunicacion.js
+++ server/router/comunicacion.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const restify = require('express-restify-mongoose')
 const router = express.Router()
+const { Agenda } = require('../utilities/agenda')
+const sendEmailNuevoMensaje = require('../utilities/agenda/send_email_nuevo_mensaje.job')
 const { deleteProp, auth, block } = require('../utilities/router')
 const { Comunicacion } = require('../models/comunicacion')
+const { Persona } = require('../models/persona')
 
 restify.serve(router, Comunicacion, {
   preDelete: block,
   preUpdate: [deleteProp, auth.isLogin], // TODO: asolo las de el, delete to and form query porps
   preCreate: [auth.isLogin], // TODO: solo las de el
+  postCreate: [
+    async (req, _, next) => {
+      try {
+        const message = req.erm.result
+        const { from, trabajo: trabajoId } = message
+        const person = await Persona.findById(from)
+        if (!person) return next()
+        sendEmailNuevoMensaje.jobCreate(Agenda, {
+          email: person.email,
+          link: process.env.FRONT_URL + '/trabajos/' + trabajoId,
+        })
+        return next()
+      } catch (err) {
+        return next(err)
+      }
+    },
+  ],
   preRead: [auth.isLogin], // TODO solo las de el
 })
 
