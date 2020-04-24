22b3d1c63170ff2420b90b7afb1fb38cf1cf23b8
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-04-23 22:32:19 -0300

    Fix redirect

--- client/middleware/authenticated.js
+++ client/middleware/authenticated.js
@@ -2,7 +2,7 @@ import Token from '~/api/Token'
 
 export default function ({ store, redirect, route }) {
   // If the user is not authenticated
-  if (route.name !== 'login' && (!store.state.user._id || !Token.get())) {
+  if (route.name !== 'login' && !store.state.user._id && !Token.get()) {
     return redirect('/login')
   }
 }
