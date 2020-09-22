8572eedad7101dc32a68886022468f80f11b6f70
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-09-22 18:32:11 -0300

    fix: load jobs for today

--- server/utilities/agenda/automatica_trabajos_para_hoy.job.js
+++ server/utilities/agenda/automatica_trabajos_para_hoy.job.js
@@ -79,16 +79,6 @@ function getUtilDate(job) {
   }
 }
 
-// function getDayOfYear(now) {
-//   const start = new Date(now.getFullYear(), 0, 0)
-//   const diff =
-//     now -
-//     start +
-//     (start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000
-//   const oneDay = 1000 * 60 * 60 * 24
-//   return Math.floor(diff / oneDay)
-// }
-
 function groupByAndFilter(trabajosList) {
   const jobByProfession = {
     /* [_id]: {
