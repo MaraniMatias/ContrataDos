f40c323678faaab479031fe91a0fe171fa534ff1
Marani Matias Ezequiel <maranimatias@gmail.com>
2020-10-01 20:56:19 -0300

    feat: improve menu bar and show user avatar on background img

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -4,48 +4,47 @@
     <v-app-bar
       fixed
       app
-      :color="hideAppBar ? 'transparent' : 'primary'"
+      dense
+      :color="hideAppBar ? 'transparent' : user.picture ? 'black' : 'primary'"
       :dark="!hideAppBar"
       :class="{ 'elevation-0': hideAppBar }"
+      :src="hideAppBar ? '' : user.picture"
     >
       <v-btn nuxt text to="/" color="transparent">
         <Logo :bg="!hideAppBar" class="mb-2" />
       </v-btn>
-      <v-toolbar-title>
-        <h1 v-show="!hideAppBar" class="headline">{{ pageTitle }}</h1>
-      </v-toolbar-title>
-      <v-spacer />
-      <v-layout justify-end align-center>
-        <template v-if="isLoggedIn">
+      <template v-if="isLoggedIn">
+        <v-layout justify-center align-center>
+          <v-btn nuxt text to="/agenda">Agenda</v-btn>
+          <v-btn nuxt text to="/trabajos">Trabajos</v-btn>
           <v-btn v-show="!hideAppBar" nuxt text icon to="/">
             <v-icon>search</v-icon>
           </v-btn>
-          <v-btn nuxt text to="/agenda">Agenda</v-btn>
-          <v-btn nuxt text to="/trabajos">Trabajos</v-btn>
+          <v-spacer />
+          <!-- <v-btn text href="/docs" target="_blank"> Ayuda</v-btn> -->
+          <v-btn text href="/docs" target="_blank" icon>
+            <v-icon>help_outline</v-icon>
+          </v-btn>
           <v-btn nuxt text to="/markers">Marcadores</v-btn>
           <v-btn nuxt text to="/perfil">Perfil</v-btn>
-          <v-btn text href="/docs">Ayuda</v-btn>
-          <!--
-          <v-btn icon @click.stop="clipped = !clipped">
-            <v-icon>mdi-application</v-icon>
-          </v-btn>
-          -->
           <v-menu offset-y bottom>
             <template v-slot:activator="{ on: menu }">
               <Avatar class="ml-4" :src="user.picture" :on="menu" />
             </template>
             <v-list class="mt-2">
+              <v-list-item>{{ headline }}</v-list-item>
               <v-list-item @click="loginOut()">Cerrar Sesión</v-list-item>
             </v-list>
           </v-menu>
-        </template>
-        <template v-else>
-          <v-btn text href="/docs">Ayuda</v-btn>
-          <v-btn depressed class="ml-4" color="primary" @click="goToPerfil">
-            Iniciar session
-          </v-btn>
-        </template>
-      </v-layout>
+        </v-layout>
+      </template>
+      <template v-else>
+        <v-spacer />
+        <v-btn text href="/docs" target="_blank"> Ayuda</v-btn>
+        <v-btn depressed class="ml-4" color="primary" @click="goToPerfil">
+          Iniciar session
+        </v-btn>
+      </template>
     </v-app-bar>
     <v-container class="fill-height" fluid>
       <nuxt />
@@ -63,6 +62,7 @@ import Snackbar from '~/components/Snackbar.vue'
 import Logo from '~/components/Logo.vue'
 import Avatar from '~/components/Avatar.vue'
 import Copyright from '~/components/Copyright.vue'
+import camelCase from '~~/server/utilities/capitalizeWords'
 
 export default {
   components: { Copyright, Avatar, Logo, Snackbar },
@@ -73,6 +73,15 @@ export default {
     ...mapGetters(['isLoggedIn']),
     user() {
       return this.$store.state.user || {}
+    },
+    headline() {
+      const nombre = this.user.nombre || ''
+      const apellido = this.user.apellido || ''
+      return camelCase(
+        this.user.razon_social
+          ? this.razon_social + `(${nombre} ${apellido})`
+          : nombre + ' ' + apellido
+      )
     },
     hideAppBar() {
       return ['index', 'login', 'loginout', 'singup'].includes(this.$route.name)
@@ -124,5 +133,13 @@ export default {
 <style>
 ng-cloak {
   display: none !important;
+}
+#app.v-application.v-application--is-ltr.theme--light
+  div.v-application--wrap
+  header.v-sheet.v-sheet--tile.theme--dark.v-toolbar.v-toolbar--dense.v-app-bar.v-app-bar--fixed.black
+  div.v-toolbar__image
+  div.v-responsive.v-image
+  div.v-image__image.v-image__image--cover {
+  opacity: 70%;
 }
 </style>
